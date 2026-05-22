f63b1ba36a3fae9be9521a396398a20dbbb919fd
Rishabh Shukla <rishabhshukla86024@gmail.com>
2026-05-22 21:11:02 +0530

    chore: configure static export for render

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export', // <-- Yeh line add karni hai
+  images: {
+    unoptimized: true, // Static export ke liye image optimization off karna padta hai
+  },
 };
 
 export default nextConfig;
